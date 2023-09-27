14a3599c4d728315c60416fea7d9719923a00662
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-09-27 14:12:58 -0500

    Display more data

--- src/getCurrentData.js
+++ src/getCurrentData.js
@@ -1,11 +1,11 @@
-const baseUrl = 'https://api.weatherapi.com/v1/current.json?key=';
+const baseUrl = 'https://api.weatherapi.com/v1/forecast.json?key=';
 /**
  * Gets data
  * @param {string} city defines which city to get data for
  * @param {string} key = weatherapi key
  */
 async function getCurrentData(city, key) {
-  const fullUrl = `${baseUrl}${key}&q=${city}&aqi=no`;
+  const fullUrl = `${baseUrl}${key}&q=${city}&days=3&aqi=no`;
   const result = await fetch(fullUrl, {mode: 'cors'});
   const data = await result.json();
   return data;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,28 @@ const b = document.createElement('button');
 b.innerText = 'Click Me!';
 document.body.appendChild(b);
 const p = document.createElement('p');
-document.body.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.innerText = 'Forecast:';
+const p2 = document.createElement('p');
 
 b.addEventListener('click', async () => {
   const data = await getCurrentData(prompt('City?'), key);
   p.innerText = `City: ${data.location.name}, ${data.location.country}`;
   p.innerHTML += `<br>Current temperature: ${data.current.temp_c} °C`;
+  p.innerHTML += `, feels like: ${data.current.feelslike_c} °C`;
+  p.innerHTML += `<br>Current conditions: ${data.current.condition.text}`;
+  let iconUrl = [...data.current.condition.icon];
+  iconUrl.splice(0, 2);
+  iconUrl = 'https://' + iconUrl.join('');
+  p.innerHTML += `<br><img src='${iconUrl}'>`;
+  p.innerHTML += `<br>Humidity: ${data.current.humidity}%`;
+  p.innerHTML += ` | Wind speed: ${data.current.wind_kph} km/h`;
+
+  p2.innerText = `${data.forecast.forecastday[1].date}`;
+  p2.innerHTML += `<br>Max temp: ${data.forecast.forecastday[1].day.maxtemp_c}`;
+  p2.innerHTML += `<br>Min temp: ${data.forecast.forecastday[1].day.mintemp_c}`;
+  document.body.appendChild(p);
+  document.body.appendChild(h3);
+  document.body.appendChild(p2);
 });
