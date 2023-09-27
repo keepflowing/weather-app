852bf056726453064484547d3ba5c5e2da5f0878
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-09-27 13:25:26 -0500

    Create basic connection to weatherapi

---
+++ src/getCurrentData.js
@@ -0,0 +1,14 @@
+const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=';
+/**
+ * Gets data
+ * @param {string} city defines which city to get data for
+ * @param {string} key = weatherapi key
+ */
+async function getCurrentData(city, key) {
+  const fullUrl = `${baseUrl}${key}&q=${city}&aqi=no`;
+  const result = await fetch(fullUrl, {mode: 'cors'});
+  const data = await result.json();
+  return data;
+}
+
+export default getCurrentData;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log('hello world');
-alert('hey');
+import getCurrentData from './getCurrentData';
+// weatherapi key
+const key = 'c6bac9574134424aa7014242232309';
+
+const b = document.createElement('button');
+b.innerText = 'Click Me!';
+document.body.appendChild(b);
+const p = document.createElement('p');
+document.body.appendChild(p);
+
+b.addEventListener('click', async () => {
+  const data = await getCurrentData(prompt('City?'), key);
+  p.innerText = `City: ${data.location.name}, ${data.location.country}`;
+  p.innerHTML += `<br>Current temperature: ${data.current.temp_c} °C`;
+});
